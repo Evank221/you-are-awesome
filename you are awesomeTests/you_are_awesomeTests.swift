bf2a306e6c9aa74041fc797d2e5396fa74035d4a
Evank221 <evanharo49@gmail.com>
2025-11-19 10:50:43 -0800

    animation update 1

--- you are awesomeTests/you_are_awesomeTests.swift
+++ you are awesomeTests/you_are_awesomeTests.swift
@@ -9,7 +9,6 @@ import Testing
 @testable import you_are_awesome
 
 struct you_are_awesomeTests {
-
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     }
